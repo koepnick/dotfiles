9e3aa299c877add7d99dc3d4b58185cc105eecb1
Sam Koepnick <sam@koepnick.io>
2023-07-08 19:22:42 -0600

    Sway

--- sway/startup.sh
+++ sway/startup.sh
@@ -1,1 +1,5 @@
-exec rm -f /tmp/sovpipe && mkfifo /tmp/sovpipe && tail -f /tmp/sovpipe | sov -t 500
+#!/bin/bash
+# exec rm -f /tmp/sovpipe && mkfifo /tmp/sovpipe && tail -f /tmp/sovpipe | sov -t 500
+systemctl --user start avizo
+systemctl --user start kanshi
+/sbin/waybar &
