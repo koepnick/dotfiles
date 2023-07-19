19bc1d840824193a4b38c89b2aa57d73a9e36e3d
Sam Koepnick <sam@koepnick.io>
2023-07-19 15:48:25 -0600

    Ripgrep quote fix

--- bash/environment.sh
+++ bash/environment.sh
@@ -1,4 +1,4 @@
 export EDITOR=/usr/bin/nvim
-export NPM_CONFIG_USERCONFIG="~/.config/npm/npmrc"
+export NPM_CONFIG_USERCONFIG=~/.config/npm/npmrc
 export DOCKER_BUILDKIT=1
-export RIPGREP_CONFIG_PATH="~/.config/ripgrep/config"
+export RIPGREP_CONFIG_PATH=~/.config/ripgrep/config
