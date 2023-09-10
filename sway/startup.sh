a594e7108b1690c400806dbb2d6e540e49415b00
Sam Koepnick <sam@koepnick.io>
2023-09-10 17:34:22 -0600

    Sway wl-paste

--- sway/startup.sh
+++ sway/startup.sh
@@ -2,16 +2,16 @@
 # exec rm -f /tmp/sovpipe && mkfifo /tmp/sovpipe && tail -f /tmp/sovpipe | sov -t 500
 #systemctl --user start avizo
 #systemctl --user start kanshi
-pgrep -x waybar || /usr/bin/waybar > ~/.local/logs/waybar.log &
-pgrep -x avizo-service || /usr/bin/avizo-service > ~/.local/logs/avizo.log &
+pgrep -x waybar || waybar > ~/.local/logs/waybar.log &
+pgrep -x avizo-service || avizo-service > ~/.local/logs/avizo.log &
 pgrep -x wl-paste || wl-paste --watch cliphist store > ~/.local/logs/wl-paste.log &
 
 pgrep -x pia-client || /opt/piavpn/bin/pia-client > ~/.local/logs/pia-client.log &
 pgrep -x wireguird || /opt/wireguird/wireguird > ~/.local/logs/wireguird.log &
-pgrep -x kdeconnect-indicator || /usr/bin/kdeconnect-indicator > ~/.local/logs/kdeindicator.log &
-pgrep -x seafile-applet || /usr/bin//seafile-applet > ~/.local/logs/seafile-client.log &
+pgrep -x kdeconnect-indicator || kdeconnect-indicator > ~/.local/logs/kdeindicator.log &
+pgrep -x seafile-applet || seafile-applet > ~/.local/logs/seafile-client.log &
 
-pgrep -x dunst || /usr/local/bin/dunst > ~/.local/logs/dunst.log &
-pgrep -x kanshi || /usr/local/bin/kanshi > ~/.local/logs/kanshi.log &
-pgrep -x flashfocus || /usr/bin/flashfocus  > ~/.local/logs/flashfocus.log &
-pgrep -x protonmail-bridge || /usr/bin/protonmail-bridge > ~/.local/logs/protonmail-bridge.log &
+pgrep -x dunst || dunst > ~/.local/logs/dunst.log &
+pgrep -x kanshi || kanshi > ~/.local/logs/kanshi.log &
+pgrep -x flashfocus || flashfocus  > ~/.local/logs/flashfocus.log &
+pgrep -x protonmail-bridge || protonmail-bridge > ~/.local/logs/protonmail-bridge.log &
